f8acf6e1af24704f890b0dfdafee0c52d0b93305
Misqke <misqke.rust@yahoo.com>
2022-02-18 23:02:34 -0500

    hero update styling

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,25 +6,23 @@ const Hero = () => {
   return (
     <div className={styles.container} id="hero">
       <div className={styles.wrapper}>
-        <div className={`${styles.box} ${styles.left}`}>
+        <div className={styles.text_box}>
           <h1>
             Hi, i&#39;m <span>Mike</span>,
-          </h1>
-          <h1>
+            <br />
             I&#39;m a web <span>developer</span>.
           </h1>
         </div>
-        <div className={`${styles.box} ${styles.right}`}>
-          <div className={styles.img_container}>
-            <Image
-              src="/imgs/me3.png"
-              alt=""
-              height={800}
-              width={533}
-              priority
-            ></Image>
-          </div>
-          <div className={styles.blur}></div>
+        <div className={styles.img_box}>
+          <Image
+            src="/imgs/me3.png"
+            alt=""
+            height={800}
+            width={533}
+            priority
+          ></Image>
+          <div className={styles.blurY}></div>
+          <div className={styles.blurX}></div>
         </div>
       </div>
     </div>
