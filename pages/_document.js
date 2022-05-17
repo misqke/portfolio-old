a71d8883c512974fd60a33a03eb5d837e4ccc787
Misqke <misqke.rust@yahoo.com>
2022-05-17 09:27:03 -0400

    fix document head

--- pages/_document.js
+++ pages/_document.js
@@ -4,15 +4,19 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="description"
           content="portfolio website for Mike Rust, a web developer"
         />
         <link rel="icon" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond&family=Courgette&family=Patrick+Hand&display=swap"
           rel="stylesheet"
