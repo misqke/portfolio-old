8feb76872c42803fa3bcaaf6c6016a98b3b23620
Misqke <misqke.rust@yahoo.com>
2022-05-17 09:33:24 -0400

    update for deploy

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import styles from "../styles/Home.module.scss";
-import Hero from "../components/hero/Hero";
-import ProjectsSection from "../components/projects/ProjectsSection";
+import Hero from "../components/Hero/Hero";
+import ProjectsSection from "../components/Projects/ProjectsSection";
 import About from "../components/About";
 import Contact from "../components/Contact";
-import Nav from "../components/layout/Nav";
-import Footer from "../components/layout/Footer";
-import Head from "next/dist/shared/lib/head";
+import Nav from "../components/Layout/Nav";
+import Footer from "../components/Layout/Footer";
+import Head from "next/head";
 
 export default function Home() {
   return (
